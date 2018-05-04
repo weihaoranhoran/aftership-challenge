4a8d36f7bf73057b15f40f15ee13d00817b5b537
weihaoranhoran <31895702+weihaoranhoran@users.noreply.github.com>
2018-05-04 13:55:05 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const MovieCtrl=require('../controllers')
-
-router.get('/movie',MovieCtrl.searchMovies)
-router.get('/movie/:id',MovieCtrl.getLocations)
-
-module.exports = router;
